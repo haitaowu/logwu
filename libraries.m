d6125bc9c16a5c83b496867e109d22da1ec19e6b
haitaowu <whatanost@gmail.com>
2014-05-17 15:10:12 +0800

    library file

---
+++ libraries.m
@@ -0,0 +1,4 @@
+1.SVProgressHUD show the status of indeterminate tasks
+	https://github.com/samvermette/SVProgressHUD
+2.SVPullToRefresh  (or keyword pulltorefresh)
+	https://github.com/samvermette/SVPullToRefresh
