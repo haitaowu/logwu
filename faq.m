8432bca2d85617bcd87b86189ec641cc995aca29
haitaowu <whatanost@gmail.com>
2014-05-20 17:32:50 +0800

    add

---
+++ faq.m
@@ -0,0 +1,1 @@
+1.ld: 2 duplicate symbols for architecture armv7  :duplicate filename(same filename) 
--- libraries.m
+++ libraries.m
@@ -2,3 +2,8 @@
 	https://github.com/samvermette/SVProgressHUD
 2.SVPullToRefresh  (or keyword pulltorefresh)
 	https://github.com/samvermette/SVPullToRefresh
+3.umeng
+4.Google-Mobile-Ads-SDK 
+	Homepage: https://developers.google.com/mobile-ads-sdk/
+5.GoogleAnalytics-iOS-SDK
+	Homepage: https://developers.google.com/analytics/devguides/collection/ios/
