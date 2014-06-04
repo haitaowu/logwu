24f9814385fc193e7d785dcceb453c7b3ccabeee
haitaowu <whatanost@gmail.com>
2014-06-04 09:42:04 +0800

    add

--- libraries.m
+++ libraries.m
@@ -1,9 +1,32 @@
 1.SVProgressHUD show the status of indeterminate tasks
 	https://github.com/samvermette/SVProgressHUD
+
 2.SVPullToRefresh  (or keyword pulltorefresh)
 	https://github.com/samvermette/SVPullToRefresh
+
 3.umeng
+
 4.Google-Mobile-Ads-SDK 
 	Homepage: https://developers.google.com/mobile-ads-sdk/
+
 5.GoogleAnalytics-iOS-SDK
 	Homepage: https://developers.google.com/analytics/devguides/collection/ios/
+
+6.Mantle Model framwork for Cocoa and Cocoa Touch.
+	 turn JSON data into NSObject subclasses
+	pod 'Mantle', '~> 1.5'
+	Homepage: https://github.com/Mantle/Mantle
+	
+7.LBBlurredImage is an UIImageView category that allow to set an image and make this blurred.
+	Homepage: https://github.com/lukabernardi/LBBlurredImage
+
+8.TSMessages (0.9.9)
+   Easy to use and customizable messages/notifications for iOS à la Tweetbot.
+   pod 'TSMessages', '~> 0.9.9'
+   - Homepage: https://github.com/toursprung/TSMessages/
+
+9.-> ReactiveCocoa (2.3.1)
+   A framework for composing and transforming streams of values.
+   pod 'ReactiveCocoa', '~> 2.3.1'
+   - Homepage: https://github.com/blog/1107-reactivecocoa-is-now-open-source
+   - Source:   https://github.com/ReactiveCocoa/ReactiveCocoa.git
