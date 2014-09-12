373dc91e876301e32ff5f082b55761e44a7f9a9f
haitaowu <whatanost@gmail.com>
2014-09-12 21:10:04 +0800

    add faq

--- faq.m
+++ faq.m
@@ -1,1 +1,6 @@
 1.ld: 2 duplicate symbols for architecture armv7  :duplicate filename(same filename) 
+2.fatal: unable to access push
+	answer: Edit .git/config file under your repo directory
+		Find url= entry under section [remote "origin"]
+		Change it from url=https://github.com/rootux/ms-Dropdown.git to https://USERNAME@github.com/rootux/ms-Dropdown.git
+		where USERNAME is your github user name
